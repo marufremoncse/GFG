57204bdbc782d72888d924b04aee6e88136fc489
marufremoncse <marufremon.cse14.2@gmail.com>
2024-07-15 22:55:18 +0600

    print a linked List

---
+++ src/linkedList/PrintALinkedList.java
@@ -0,0 +1,20 @@
+package linkedList;
+
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class PrintALinkedList {
+    void display(Node head) {
+        // add code here.
+        while(head!=null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+}
