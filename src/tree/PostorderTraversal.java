e43403436cdb5ff501667a3dac4d56578684a558
marufremoncse <marufremon.cse14.2@gmail.com>
2024-07-26 01:25:53 +0600

    Postorder Traversal

--- src/tree/PostorderTraversal.java
+++ src/tree/PostorderTraversal.java
@@ -1,2 +1,26 @@
-package tree;public class PostorderTraversal {
+package tree;
+
+import java.util.ArrayList;
+
+public class PostorderTraversal {
+    ArrayList<Integer> ls = new ArrayList<>();
+    // Function to return a list containing the inorder traversal of the tree.
+    ArrayList<Integer> postOrder(Node root) {
+        // Code
+        if(root==null){
+            return ls;
+        }
+
+        if(root.left!=null){
+            postOrder(root.left);
+        }
+
+        if(root.right!=null){
+            postOrder(root.right);
+        }
+
+        ls.add(root.data);
+
+        return ls;
+    }
 }
