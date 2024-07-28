b16f6095bae803931ecbdab8985ba554d8882883
marufremoncse <marufremon.cse14.2@gmail.com>
2024-07-28 20:17:50 +0800

    Anagram Palindrome

---
+++ src/sorting/AnagramPalindrome.java
@@ -0,0 +1,25 @@
+package sorting;
+
+public class AnagramPalindrome {
+    int isPossible (String S)
+    {
+        // your code here
+        int countOdd = 0;
+        int[] ara = new int[26];
+
+        for(char c:S.toCharArray()){
+            ara[c-97]++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(ara[i]%2==1){
+                countOdd++;
+                if(countOdd>=2){
+                    return 0;
+                }
+            }
+        }
+
+        return 1;
+    }
+}
